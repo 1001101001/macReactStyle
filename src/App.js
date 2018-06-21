19e201264fc7f19a0b7212a9791c30eee6ec3d97
voakgh <victorthinkahead@gmail.com>
2018-06-21 03:33:18 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Window, TitleBar, Text, ProgressCircle } from 'react-desktop/';
+
+export default class extends Component {
+  render() {
+    return (
+      <Window
+        chrome
+        height="300px"
+        padding="10px"
+      >
+        <TitleBar title="untitled text 5" controls/>
+        <h1>macOS style window in react!</h1>
+
+      </Window>
+    );
+  }
+}
